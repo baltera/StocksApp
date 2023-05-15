87b9a36c0d98d2964150cf0a496f5c6afd849f66
Edgardo Ramirez <edgardo.ramirez94@gmail.com>
2023-05-15 14:54:33 -0400

    Consume and log API endpoint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'StocksApp';
+  stocks: any;
+
+  constructor(private _httpClient: HttpClient) {}
+  
+  ngOnInit(): void {
+    this._httpClient.get(environment.apiURL).subscribe({
+      next: response => {
+        console.log(response);
+        this.stocks = response;
+      },
+      error: e => console.error(e),
+      complete: () => console.log("completed!")
+    });
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,3 @@
-export const environment = {};
+export const environment = {
+    apiURL: "https://localhost:7235/api/Stocks?tickers=TSLA"
+};
