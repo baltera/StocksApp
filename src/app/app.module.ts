f607f28c15ada4d479a72c71167e99f9d5d75001
Edgardo Ramirez <edgardo.ramirez94@gmail.com>
2023-05-16 16:05:49 -0400

    Create dummy values for flex layout approach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { StockComponent } from './stock/stock.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { MaterialModule } from './material/material.module';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 
-const MaterialComponents: any = [MatToolbarModule];
+const MaterialComponents: any = [MatToolbarModule, MatCardModule];
 
 @NgModule({
   imports: [MaterialComponents],
