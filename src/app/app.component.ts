148c033a3ef8c01d560a356b26c6300ba317865f
Edgardo Ramirez <edgardo.ramirez94@gmail.com>
2023-05-16 17:04:08 -0400

    Format card with emulated response from API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,19 +8,22 @@ import { environment } from 'src/environments/environment.development';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  _DATA: string = '[ { "id": 0, "name": "Tesla Inc", "symbol": "TSLA", "lastSalePrice": 169.23, "percentChange": 1.69, "marketCap": 529004658688, "quoteId": 0 }, { "id": 0, "name": "Microsoft Corporation", "symbol": "MSFT", "lastSalePrice": 313.65, "percentChange": 1.32, "marketCap": 2325861105664, "quoteId": 0 }, { "id": 0, "name": "CGI Inc", "symbol": "GIB", "lastSalePrice": 104.03, "percentChange": 0.49, "marketCap": 24387960832, "quoteId": 0 } ]';
   title = 'StocksApp';
   stocks: any;
 
   constructor(private _httpClient: HttpClient) {}
   
   ngOnInit(): void {
-    this._httpClient.get(environment.apiURL).subscribe({
-      next: response => {
-        console.log(response);
-        this.stocks = response;
-      },
-      error: e => console.error(e),
-      complete: () => console.log("completed!")
-    });
+    this.stocks = JSON.parse(this._DATA);    
+    console.log(this.stocks);
+    // this._httpClient.get(environment.apiURL).subscribe({
+    //   next: response => {
+    //     console.log(response);
+    //     this.stocks = response;
+    //   },
+    //   error: e => console.error(e),
+    //   complete: () => console.log("completed!")
+    // });
   }
 }
